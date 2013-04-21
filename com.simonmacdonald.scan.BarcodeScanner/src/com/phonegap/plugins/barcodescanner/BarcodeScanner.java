420946ad92e2e8700aace0a81afb99d4ac47cecf
Robbertvanderhulst <robbertvanderhulst@hotmail.com>
2013-04-21 17:45:00 +0200

    Comentaar toegevoegd

--- com.simonmacdonald.scan.BarcodeScanner/src/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ com.simonmacdonald.scan.BarcodeScanner/src/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -1,9 +1,13 @@
 /**
- * PhoneGap is available under *either* the terms of the modified BSD license *or* the
- * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
- *
- * Copyright (c) Matt Kane 2010
- * Copyright (c) 2011, IBM Corporation
+* @title Moyee Barcodescanner
+* @author Robbert van der Hulst
+* @param S1063848
+* @since 04-04-2013
+* @version 1.0
+*/
+
+/**
+ * Naam van de package.
  */
 
 package com.phonegap.plugins.barcodescanner;
@@ -20,8 +24,9 @@ import org.apache.cordova.api.Plugin;
 import org.apache.cordova.api.PluginResult;
 
 /**
- * This calls out to the ZXing barcode reader and returns the result.
+ * De volgende klasse roept de ZXing barcodelezer op en geeft hierna het resultaat.
  */
+
 public class BarcodeScanner extends Plugin {
     private static final String SCAN = "scan";
     private static final String ENCODE = "encode";
@@ -38,25 +43,24 @@ public class BarcodeScanner extends Plugin {
     private static final String EMAIL_TYPE = "EMAIL_TYPE";
     private static final String PHONE_TYPE = "PHONE_TYPE";
     private static final String SMS_TYPE = "SMS_TYPE";
-
     public static final int REQUEST_CODE = 0x0ba7c0de;
-
     public String callback;
 
     /**
-     * Constructor.
+     * De constructor van de applicatie.
      */
     public BarcodeScanner() {
     }
 
     /**
-     * Executes the request and returns PluginResult.
+     * Voert de scanner uit en stuurt de plugin resultaten terug. 
      *
-     * @param action        The action to execute.
-     * @param args          JSONArray of arguments for the plugin.
-     * @param callbackId    The callback id used when calling back into JavaScript.
-     * @return              A PluginResult object with a status and message.
+     * @param action        De actie wordt hiermee uitgevoerd.
+     * @param args          JSONArray argumenten voor de plugin.
+     * @param callbackId    Dit id wordt terug gestuurd bij het gebruiken van javascript.
+     * @return              Het plugin resultaat wordt terug gestuurd met een status en een boodschap.
      */
+    
     public PluginResult execute(String action, JSONArray args, String callbackId) {
         this.callback = callbackId;
 
@@ -66,18 +70,35 @@ public class BarcodeScanner extends Plugin {
                 String type = obj.optString(TYPE);
                 String data = obj.optString(DATA);
 
-                // If the type is null then force the type to text
+                /**
+                 * 
+                 * Als het type 0 is dan wordt het omgezet naar tekst. 
+                 * 
+                 */
+                
                 if (type == null) {
                     type = TEXT_TYPE;
                 }
+                
+                /**
+                 * 
+                 * Als de data 0 is dan wordt er een foutmelding weergegeven.
+                 * 
+                 */
 
                 if (data == null) {
-                    return new PluginResult(PluginResult.Status.ERROR, "User did not specify data to encode");
+                    return new PluginResult(PluginResult.Status.ERROR, "Er zijn geen gegevens opgegeven om te coderen");
                 }
-
+                
+                /**
+                 * 
+                 * Als de data 0 is dan wordt er een foutmelding weergegeven.
+                 * 
+                 */
+                
                 encode(type, data);
             } else {
-                return new PluginResult(PluginResult.Status.ERROR, "User did not specify data to encode");
+                return new PluginResult(PluginResult.Status.ERROR, "Er zijn geen gegevens opgegeven om te coderen");
             }
         }
         else if (action.equals(SCAN)) {
@@ -90,24 +111,22 @@ public class BarcodeScanner extends Plugin {
         return r;
     }
 
-
     /**
-     * Starts an intent to scan and decode a barcode.
+     * Start een activiteit om te scannen en te coderen van een barcode.
      */
+    
     public void scan() {
         Intent intentScan = new Intent(SCAN_INTENT);
         intentScan.addCategory(Intent.CATEGORY_DEFAULT);
-
         this.cordova.startActivityForResult((Plugin) this, intentScan, REQUEST_CODE);
     }
 
     /**
-     * Called when the barcode scanner intent completes
+     * Roept de barcode scanner op
      *
-     * @param requestCode       The request code originally supplied to startActivityForResult(),
-     *                          allowing you to identify who this result came from.
-     * @param resultCode        The integer result code returned by the child activity through its setResult().
-     * @param intent            An Intent, which can return result data to the caller (various data can be attached to Intent "extras").
+     * @param requestCode       Roept de code op die gescant is met de scanner
+     * @param resultCode       	Laat het resultaat zien van de code.
+     * @param intent            De gegevens worden teruggestuurd naar de gebruiker. 
      */
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_CODE) {
@@ -118,7 +137,7 @@ public class BarcodeScanner extends Plugin {
                     obj.put(FORMAT, intent.getStringExtra("SCAN_RESULT_FORMAT"));
                     obj.put(CANCELLED, false);
                 } catch(JSONException e) {
-                    //Log.d(LOG_TAG, "This should never happen");
+                    
                 }
                 this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
             } if (resultCode == Activity.RESULT_CANCELED) {
@@ -128,7 +147,7 @@ public class BarcodeScanner extends Plugin {
                     obj.put(FORMAT, "");
                     obj.put(CANCELLED, true);
                 } catch(JSONException e) {
-                    //Log.d(LOG_TAG, "This should never happen");
+                   
                 }
                 this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
             } else {
@@ -138,8 +157,8 @@ public class BarcodeScanner extends Plugin {
     }
 
     /**
-     * Initiates a barcode encode.
-     * @param data  The data to encode in the bar code
+     * Kan een barcode encoderen
+     * @param data  
      * @param data2
      */
     public void encode(String type, String data) {
